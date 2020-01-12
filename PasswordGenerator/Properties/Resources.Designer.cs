b4f897f48f05b5831220eed5f69a5742a0c4deb6
Василий Саурин <saurin048@ya.ru>
2020-01-12 17:23:00 +0100

    added new icon

--- PasswordGenerator/Properties/Resources.Designer.cs
+++ PasswordGenerator/Properties/Resources.Designer.cs
@@ -63,9 +63,9 @@ namespace PasswordGenerator.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
-        internal static System.Drawing.Icon icon {
+        internal static System.Drawing.Icon hacking {
             get {
-                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                object obj = ResourceManager.GetObject("hacking", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
             }
         }
