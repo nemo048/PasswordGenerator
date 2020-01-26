162c23d08352c32199f8365d57e4311bd2cc101b
Василий Саурин <saurin048@ya.ru>
2020-01-26 09:48:04 +0100

    Made password field readonly

--- PasswordGenerator/MainForm.Designer.cs
+++ PasswordGenerator/MainForm.Designer.cs
@@ -79,6 +79,7 @@
             this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.passwordTextBox.Location = new System.Drawing.Point(9, 6);
             this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.ReadOnly = true;
             this.passwordTextBox.Size = new System.Drawing.Size(300, 72);
             this.passwordTextBox.TabIndex = 3;
             this.passwordTextBox.Text = "";
